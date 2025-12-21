4e65aed56fbed952b0a9ed64e0fd04756f3ccc3f
yasjprajapati2100 <yashprajapati2100@gmail.com>
2025-12-21 11:54:17 +0530

    all done

--- study_notes_portal/notes_portal/admin.py
+++ study_notes_portal/notes_portal/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Manager, Folder, Note
 
-# Register your models here.
+@admin.register(Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ['username', 'created_at']
+    readonly_fields = ['created_at']
+
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_by', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['name']
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'folder', 'file_type', 'uploaded_by', 'uploaded_at']
+    list_filter = ['file_type', 'uploaded_at']
+    search_fields = ['title']
